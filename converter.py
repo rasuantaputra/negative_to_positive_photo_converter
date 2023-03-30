4530e43a05870d3ea30a07a4d4c5fd86d27158cc
rasuantaputra <78709617+rasuantaputra@users.noreply.github.com>
2023-03-30 14:20:15 +0700

    initial init

--- converter.py
+++ converter.py
@@ -2,8 +2,6 @@ from PIL import Image, ImageOps
 
 # Open the negative photo
 photo = ['1.jpg', '2.jpg', '3.jpg', '4.jpg']
-negative_image = []
-
 
 for i in photo:
     ImageOps.invert(Image.open(i)).save('convert-'+i)
